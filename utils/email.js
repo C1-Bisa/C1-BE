11a93b812e9b547d653944b7a7decbcd388e7d73
MhassanS23 <maulanahassan366@gmail.com>
2023-06-06 15:40:51 +0700

    change email nodemailer

--- utils/email.js
+++ utils/email.js
@@ -22,8 +22,13 @@ exports.sendMail = (email, token) => {
         from: "'FlywUs' <no-reply@gmail.com>",
         to: email,
         subject: "OTP Code for Verification",
-        html: `<b style="text-align: center;">OTP Token</b> <br></br>
-                ${token}`
+        html: `
+        <h1 style="text-align:center;">Your Verification OTP Code</h1><br></br>
+        <p style="margin-bottom:1.5rem; text-align:center;">
+          Enter this verification code in field:
+        </p><br></br>
+        <p style="font-size:2rem; text-align:center; font-weight: 900; background:#000b76; border-radius: 0.5rem; padding:0.8rem; color:yellow;">${token}</p><br></br>
+        <p style="text-align:center; margin-top:1.5rem">Verification code is valid only for 1 minutes</p>`
       };
   
       transporter.sendMail(options, (err, info)=>{
