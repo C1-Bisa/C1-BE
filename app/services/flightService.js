bc893e30a77908ab51bc20da890139b355bca99c
ariefhk <ariefrachmanhakim1001@gmail.com>
2023-06-08 11:37:49 +0700

    chore: change yg ada digrup hehe

--- app/services/flightService.js
+++ app/services/flightService.js
@@ -1,92 +1,106 @@
 const flightRepository = require("../repositories/flightRepository");
+const airlineRepository = require("../repositories/airlineRepository");
 
 module.exports = {
+    async list() {
+        try {
+            const flight = await flightRepository.findAll();
+            const flightCount = await flightRepository.getTotalFlight();
 
-  async list() {
-    try {
-      const flight = await flightRepository.findAll();
-      const flightCount = await flightRepository.getTotalFlight();
+            return {
+                data: flight,
+                count: flightCount,
+            };
+        } catch (err) {
+            throw err;
+        }
+    },
 
-      return {
-        data: flight,
-        count: flightCount,
-      };
-    } catch (err) {
-      throw err;
-    }
-  },
+    async create(reqBody) {
+        try {
+            if (
+                !reqBody.airline_id ||
+                !reqBody.airport_id ||
+                !reqBody.departure_date ||
+                !reqBody.departure_time ||
+                !reqBody.arrival_date ||
+                !reqBody.arrival_time ||
+                !reqBody.from ||
+                !reqBody.to ||
+                !reqBody.price ||
+                !reqBody.flight_class ||
+                !reqBody.description
+            ) {
+                return {
+                    status: "Failed",
+                    message: "form must be filled!",
+                    data: null,
+                };
+            }
+            const ticket = await flightRepository.create({
+                airline_id: reqBody.airline_id,
+                airport_id: reqBody.airport_id,
+                departure_date: reqBody.departure_date,
+                departure_time: reqBody.departure_time,
+                arrival_date: reqBody.arrival_date,
+                arrival_time: reqBody.arrival_time,
+                from: reqBody.from,
+                to: reqBody.to,
+                price: reqBody.price,
+                flight_class: reqBody.flight_class,
+                description: reqBody.description,
+            });
 
-  async create(reqBody) {
-    try {
-    if(!reqBody.airline_id || !reqBody.airport_id || !reqBody.departure_date || !reqBody.departure_time || !reqBody.arrival_date || !reqBody.arrival_time || !reqBody.from || !reqBody.to || !reqBody.price || !reqBody.flight_class || !reqBody.description){
-        return {
-            status: "Failed",
-            message: "form must be filled!",
-            data: null,
-          };
-    }
-      const ticket = await flightRepository.create({
-        airline_id: reqBody.airline_id,
-        airport_id: reqBody.airport_id,
-        departure_date: reqBody.departure_date,
-        departure_time: reqBody.departure_time,
-        arrival_date: reqBody.arrival_date,
-        arrival_time: reqBody.arrival_time,
-        from: reqBody.from,
-        to: reqBody.to,
-        price: reqBody.price,
-        flight_class: reqBody.flight_class,
-        description: reqBody.description
-      });
-    
-      return {
-        status: "Success",
-        message: "Flight data successfuly create!",
-        data: ticket,
-      };
-    } catch (err) {
-      throw err;
-    }
-  },
+            return {
+                status: "Success",
+                message: "Flight data successfuly create!",
+                data: ticket,
+            };
+        } catch (err) {
+            throw err;
+        }
+    },
 
-  async update(id, reqBody) {
-    try {
-    const ticket = await flightRepository.update(id,{
-        airline_id: reqBody.airline_id,
-        airport_id: reqBody.airport_id,
-        departure_date: reqBody.departure_date,
-        departure_time: reqBody.departure_time,
-        arrival_date: reqBody.arrival_date,
-        arrival_time: reqBody.arrival_time,
-        from: reqBody.from,
-        to: reqBody.to,
-        price: reqBody.price,
-        flight_class: reqBody.flight_class,
-        description: reqBody.description
-      }).then((result) => {return result});
-    
-      return {
-        status: "Success",
-        message: "Flight data successfuly updated!",
-        data: ticket,
-      };
-    } catch (err) {
-      throw err;
-    }
-  },
+    async update(id, reqBody) {
+        try {
+            const ticket = await flightRepository
+                .update(id, {
+                    airline_id: reqBody.airline_id,
+                    airport_id: reqBody.airport_id,
+                    departure_date: reqBody.departure_date,
+                    departure_time: reqBody.departure_time,
+                    arrival_date: reqBody.arrival_date,
+                    arrival_time: reqBody.arrival_time,
+                    from: reqBody.from,
+                    to: reqBody.to,
+                    price: reqBody.price,
+                    flight_class: reqBody.flight_class,
+                    description: reqBody.description,
+                })
+                .then((result) => {
+                    return result;
+                });
 
-  async delete(id) {
-    try {
-        const ticket = await flightRepository.delete(id);
-        return {
-            status: "Success",
-            message: "Flight data successfuly deleted!",
-            data: ticket,
-          };
-    } catch (err) {
-      throw err;
-    }
-  },
+            return {
+                status: "Success",
+                message: "Flight data successfuly updated!",
+                data: ticket,
+            };
+        } catch (err) {
+            throw err;
+        }
+    },
 
-  
+    async delete(id) {
+        try {
+            const ticket = await flightRepository.delete(id);
+            return {
+                status: "Success",
+                message: "Flight data successfuly deleted!",
+                data: ticket,
+            };
+        } catch (err) {
+            throw err;
+        }
+    },
 };
